bc0db7c2fcb39c6cf079ec6675684689594b7ad4
Eric Dallo <ericdallo06@hotmail.com>
2025-11-04 19:46:06 -0300

    Finish feature

--- eca-rewrite.el
+++ eca-rewrite.el
@@ -19,15 +19,31 @@
   :group 'eca
   :type 'string)
 
+(defcustom eca-rewrite-finish-prefix "Rewrite: "
+  "The prefix text to show after a rewrite was done."
+  :group 'eca
+  :type 'string)
 
-(defcustom eca-rewrite-finished-action nil
+(defcustom eca-rewrite-diff-tool 'ediff
+  "Diff tool to use when calling diff in a rewrite overlay."
+  :group 'eca
+  :type '(choice
+          (const :tag "Simple diff" simple-diff)
+          (const :tag "Ediff" ediff)))
+
+(defcustom eca-rewrite-finished-action 'show-overlay-actions
   "Action to take after finishing rewriting a text region using ECA."
   :group 'eca
   :type '(choice
-          (const :tag "Wait user interaction" nil)
-          (const :tag "Present choices menu" menu)
+          (const :tag "Show actions above overlay" show-overlay-actions)
           (const :tag "Accept" accept)
-          (const :tag "Diff" diff)))
+          (const :tag "Diff" diff)
+          (const :tag "Merge" merge)))
+
+(defcustom eca-rewrite-on-finished-hook '()
+  "Hook to run after a ECA rewrite is finished, the overlay is the arg."
+  :type 'hook
+  :group 'eca)
 
 (defface eca-rewrite-highlight-face
   '((((class color) (min-colors 88) (background dark))
@@ -63,13 +79,11 @@
 
 (defvar-keymap eca-rewrite-actions-map
   :doc "Keymap for rewrite overlay actions."
-  "RET" #'eca-rewrite--dispatch
-  "<return>" #'eca-rewrite--dispatch
-  "C-c C-a" #'eca-rewrite--accept-at-point
-  "C-c C-r" #'eca-rewrite-reject-at-point
-  "C-c C-d" #'eca-rewrite--diff
-  "C-c C-m" #'eca-rewrite--merge
-  "C-c C-t" #'eca-rewrite--retry)
+  "a" #'eca-rewrite-accept
+  "r" #'eca-rewrite-reject
+  "d" #'eca-rewrite-diff
+  "m" #'eca-rewrite-merge
+  "t" #'eca-rewrite-retry)
 
 (defun eca-rewrite--get-buffer (id)
   "Get buffer for rewrite ID."
@@ -78,17 +92,26 @@
 (defun eca-rewrite--overlay-at-point (&optional pt)
   "Return the overlay at PT or point if found."
   (--first (overlay-get it 'eca-rewrite--id)
-           (overlays-in (line-beginning-position) (or pt (point)))))
+           (overlays-in (line-beginning-position) (or pt (1+ (point))))))
 
 (defun eca-rewrite--overlay-from-id (id)
   "Return the overlay with ID."
   (--first (string= id (overlay-get it 'eca-rewrite--id))
            (overlays-in (point-min) (point-max))))
 
+(defun eca-rewrite--time->presentable-time (ms)
+  "Return a presentable time for MS."
+  (let ((secs (/ (float ms) 1000)))
+    (format "%.2f s" secs)))
+
 (defun eca-rewrite--overlay-menu-str (ov label)
   "Return the before-string for overlay OV.
 LABEL is the base text prefix."
-  (let ((model-str (concat "[" (overlay-get ov 'eca-rewrite--model) "]\n")))
+  (let* ((ms (overlay-get ov 'eca-rewrite--total-time-ms))
+         (model-str (concat
+                     (when ms
+                       (concat "[" (eca-rewrite--time->presentable-time ms) "] "))
+                     "[" (overlay-get ov 'eca-rewrite--model) "]\n")))
     (concat
      (unless (eq (char-before (overlay-start ov)) ?\n)
        "\n")
@@ -96,9 +119,10 @@ LABEL is the base text prefix."
      (propertize " " 'display `(space :align-to (- right ,(1+ (length model-str)))))
      (propertize model-str 'face 'eca-rewrite-model-face))))
 
-(defun eca-rewrite--setup-overlay (id start end text prompt model)
+(defun eca-rewrite--setup-overlay (id start end text path prompt model)
   "Create an overlay for ID from START to END.
 TEXT is the original selected text
+PATH is the buffer file name
 PROMPT is the instructions user sent
 MODEL is the LLM model used."
   (let ((ov (make-overlay start end nil t)))
@@ -108,7 +132,7 @@ MODEL is the LLM model used."
     (overlay-put ov 'eca-rewrite--original-text text)
     (overlay-put ov 'eca-rewrite--new-text nil)
     (overlay-put ov 'eca-rewrite--temp-buffer (generate-new-buffer "*eca-rewrite*"))
-    (overlay-put ov 'eca-rewrite--last-pos nil)
+    (overlay-put ov 'eca-rewrite--path path)
     (overlay-put ov 'eca-rewrite--prompt prompt)
     (overlay-put ov 'eca-rewrite--model model)
     (overlay-put ov 'face 'eca-rewrite-highlight-face)
@@ -117,23 +141,19 @@ MODEL is the LLM model used."
     (overlay-put ov 'help-echo "ECA rewrite")
     (overlay-put ov 'before-string (eca-rewrite--overlay-menu-str
                                     ov
-                                    (propertize "ECA rewriting..." 'face 'eca-rewrite-in-progress-prefix-face)))
+                                    (propertize "Requesting LLM..." 'face 'eca-rewrite-in-progress-prefix-face)))
     (push ov eca-rewrite--overlays)
     ov))
 
-(defun eca-rewrite--show-actions (ov)
-  "Display the ready action menu for rewrite overlay OV.
-
-This updates the overlay header to show available
-keys (accept/reject/merge/diff),briefly highlights the
-region, and disposes of the temporary buffer used during streaming."
-  (let ((choices '((?a "accept") (?r "reject") (?m "merge") (?d "diff"))))
+(defun eca-rewrite--show-overlay-actions (ov)
+  "Display the actions overlay above rewrite overlay OV."
+  (let ((choices '((?a "accept") (?r "reject") (?d "diff") (?m "merge") (?t "retry"))))
     (overlay-put
      ov
      'before-string (eca-rewrite--overlay-menu-str
                      ov
                      (concat
-                      (propertize "ECA rewrite: " 'face 'eca-rewrite-ready-prefix-face)
+                      (propertize eca-rewrite-finish-prefix 'face 'eca-rewrite-ready-prefix-face)
                       (when (fboundp #'rmc--add-key-description) ;; > Emacs 29
                         (mapconcat (lambda (e) (cdr e))
                                    (mapcar #'rmc--add-key-description choices)
@@ -142,7 +162,12 @@ region, and disposes of the temporary buffer used during streaming."
     (kill-buffer (overlay-get ov 'eca-rewrite--temp-buffer))))
 
 (defun eca-rewrite--reject (ovs)
-  "Reject the rewrite overlays OVS."
+  "Reject (discard) one or more rewrite overlays.
+
+OVS may be a single overlay or a list of overlays. For each
+overlay, remove it from the internal tracking list
+`eca-rewrite--overlays' and delete it with `delete-overlay'.
+No confirmation is asked; a nil OVS is ignored. Returns nil."
   (dolist (ov (ensure-list ovs))
     (setq eca-rewrite--overlays (delq ov eca-rewrite--overlays))
     (delete-overlay ov)))
@@ -154,6 +179,133 @@ region, and disposes of the temporary buffer used during streaming."
       (goto-char (overlay-start ov))
       (delete-region (overlay-start ov) (overlay-end ov))
       (insert (overlay-get ov 'eca-rewrite--new-text)))))
+
+(defun eca-rewrite--clone-buffer-with-rewrite (ov name)
+  "Return a clone of the buffer NAME with the rewrite overlay OV applied."
+  (let ((buf (overlay-buffer ov)))
+    (with-current-buffer buf
+      (let ((min (point-min))
+            (max (point-max))
+            (pt  (point))
+            (newb (get-buffer-create name)))
+        (save-restriction
+          (widen)
+          (with-current-buffer newb
+            (erase-buffer)
+            (insert-buffer-substring buf)))
+        (with-current-buffer newb
+          (narrow-to-region min max)
+          (goto-char pt)
+          (eca-rewrite--accept ov))
+        newb))))
+
+(defun eca-rewrite--merge-simple (beg end new-text model)
+  "Produce a merge conflict region between BEG and END for NEW-TEXT.
+MODEL is the model used."
+  (goto-char end)
+  (unless (bolp) (insert "\n"))
+  (insert "=======\n" new-text "\n>>>>>>> " model)
+  (goto-char beg)
+  (unless (bolp)
+    (insert "\n"))
+  (insert-before-markers "<<<<<<< original\n"))
+
+(defun eca-rewrite--merge-git (beg end new-text model)
+  "Produce a merge conflict region between BEG and END.
+
+Merge the region with NEW-TEXT using git merge-file.
+MODEL is the model used."
+  (let ((original-temp-file (make-temp-file "eca-rewrite-merge-"))
+        (empty-temp-file (make-temp-file "eca-rewrite-merge-"))
+        (new-temp-file (make-temp-file "eca-rewrite-merge-")))
+    (unwind-protect
+        (progn
+          (write-region beg end original-temp-file)
+          (with-temp-file empty-temp-file (insert ""))
+          (with-temp-file new-temp-file (insert new-text))
+          (goto-char beg)
+          (delete-region beg end)
+          (call-process
+           "git" nil (list (current-buffer) nil) nil
+           "merge-file" "--no-diff3" "-L" "original" "-L" "Empty" "-L"
+           model "-p"
+           original-temp-file empty-temp-file new-temp-file)
+          (goto-char beg))
+      (delete-file original-temp-file)
+      (delete-file empty-temp-file)
+      (delete-file new-temp-file))))
+
+(defun eca-rewrite--merge (ov)
+  "Merge between original text and rewrite overlay OV."
+  (when-let* ((ov-buf (overlay-buffer ov))
+              ((buffer-live-p ov-buf)))
+    (with-current-buffer ov-buf
+      (let ((model (overlay-get ov 'eca-rewrite--model))
+            (changed))
+        (save-excursion
+          (when-let* ((new-text (overlay-get ov 'eca-rewrite--new-text)))
+            (if (executable-find "git")
+                (eca-rewrite--merge-git (overlay-start ov) (overlay-end ov) new-text model)
+              (eca-rewrite--merge-simple (overlay-start ov) (overlay-end ov) new-text model))
+            (setq changed t)))
+        (when changed (smerge-mode 1)))
+      (eca-rewrite--reject (list ov)))))
+
+(defun eca-rewrite--simple-diff (ov)
+  "Show diff between original text and rewrite overlay OV."
+  (let* ((ov-buf (overlay-buffer ov))
+         (newbuf (eca-rewrite--clone-buffer-with-rewrite ov "*eca-rewrite-diff*"))
+         (diff-buf (diff-no-select ov-buf newbuf)))
+    (with-current-buffer diff-buf
+      (setq-local diff-jump-to-old-file t))
+    (display-buffer diff-buf))))
+
+(defun eca-rewrite--ediff (ov)
+  "Show diff between original text and rewrite overlay OV."
+  (let ((ov-buf (overlay-buffer ov))
+        (newbuf (eca-rewrite--clone-buffer-with-rewrite ov "*eca-rewrite-ediff*"))
+        (orig-win (current-window-configuration))
+        (hide-show-fn (lambda (&optional restore)
+                        (let ((disp (overlay-get ov 'display))
+                              (stored (overlay-get ov 'eca-rewrite--ediff)))
+                          (overlay-put ov 'face (and restore 'eca-rewrite-highlight-face))
+                          (overlay-put ov 'display (and restore stored))
+                          (overlay-put ov 'eca-rewrite--ediff (unless restore disp)))))
+        (on-quit-fn (lambda ()
+                      (when (window-configuration-p orig-win)
+                        (set-window-configuration orig-win))
+                      (funcall hide-show-fn t)
+                      (remove-hook 'ediff-quit-hook on-quit-fn))))
+    (funcall hide-show-fn)
+    (add-hook 'ediff-quit-hook on-quit-fn t)
+    (let ((ediff-window-setup-function #'ediff-setup-windows-plain)
+          (ediff-split-window-function #'split-window-horizontally))
+      (ediff-buffers ov-buf newbuf))))
+
+(defun eca-rewrite--diff (ov)
+  "Show diff between original text and rewrite overlay OV."
+  (pcase eca-rewrite-diff-tool
+    ('simple-diff (eca-rewrite--simple-diff ov))
+    ('ediff (eca-rewrite--ediff ov))
+    (_ (user-error (format "Unknown diff tool for `eca-rewrite-diff-tool': %s" eca-rewrite-diff-tool)))))
+
+(defun eca-rewrite--retry (ov session)
+  "Retry rewrite overlay OV for SESSION."
+  (let ((text (overlay-get ov 'eca-rewrite--original-text))
+        (path (overlay-get ov 'eca-rewrite--path))
+        (prompt (overlay-get ov 'eca-rewrite--prompt))
+        (start (overlay-start ov))
+        (end (overlay-end ov)))
+    (eca-rewrite--rewrite ov session text path start end prompt)))
+
+(defun eca-rewrite--rewrite (ov session text path start end prompt)
+  "Start the rewrite for OV, SESSION, TEXT, PATH, START, END and PROMPT."
+  (setq eca-rewrite--last-prompt prompt)
+  (when ov (eca-rewrite--reject ov))
+  (eca-rewrite--send session text path start end prompt)
+  (unless (get-char-property (point) 'eca-rewrite--id)
+    (when (= (point) (region-end)) (backward-char 1)))
+  (deactivate-mark))
 
 (defun eca-rewrite--send (session text path start end prompt)
   "Send rewrite request for SESSION and TEXT with PROMPT.
@@ -181,9 +333,16 @@ PATH is the optional file path."
      (lambda (res)
        (eca-info "Rewriting...")
        (setq eca-rewrite--id->buffer (eca-assoc eca-rewrite--id->buffer id (current-buffer)))
-       (eca-rewrite--setup-overlay id start end text prompt (plist-get res :model)))
+       (eca-rewrite--setup-overlay id start end text path prompt (plist-get res :model)))
      :error-callback
      (lambda (err) (eca-error "Rewrite error: %s" err)))))
+
+(defun eca-rewrite--set-reasoning (ov)
+  "Set OV overlay header to indicate reasoning is in progress."
+  (overlay-put ov 'before-string
+               (eca-rewrite--overlay-menu-str
+                ov
+                (propertize "LLM reasoning..." 'face 'eca-rewrite-in-progress-prefix-face))))
 
 (defun eca-rewrite--add-text (ov text)
   "Add TEXT as the rewritten content to overlay OV.
@@ -226,14 +385,18 @@ accepts, shows menu, or diffs according to `eca-rewrite-finished-action',
       (with-current-buffer buffer
         (let ((ov (eca-rewrite--overlay-from-id id)))
           (pcase (plist-get content :type)
+            ("reasoning" (eca-rewrite--set-reasoning ov))
             ("text" (eca-rewrite--add-text ov (plist-get content :text)))
-            ("finished" (if eca-rewrite-finished-action
-                            (pcase eca-rewrite-finished-action
-                              ('accept (eca-rewrite--accept ov))
-                              ('diff (eca-rewrite--diff ov))
-                              ('menu (eca-rewrite--menu ov))
-                              (_ (user-error (format "Uknown rewrite action '%s' for eca-rewrite-finished-action" eca-rewrite-finished-action))))
-                          (eca-rewrite--show-actions ov)))))))))
+            ("finished" (progn
+                          (overlay-put ov 'eca-rewrite--total-time-ms (plist-get content :totalTimeMs))
+                          (pcase eca-rewrite-finished-action
+                            ('accept (eca-rewrite--accept ov))
+                            ('diff (eca-rewrite--diff ov))
+                            ('merge (eca-rewrite--merge ov))
+                            ('show-overlay-actions (eca-rewrite--show-overlay-actions ov))
+                            (_ (user-error (format "Uknown rewrite action '%s' for eca-rewrite-finished-action" eca-rewrite-finished-action))))
+                          (with-demoted-errors "eca-rewrite-on-finished-hook: %S"
+                            (run-hook-with-args 'eca-rewrite-on-finished-hook ov))))))))))
 
 ;;;###autoload
 (defun eca-rewrite (prompt)
@@ -247,8 +410,7 @@ PROMPT is the instructions prompt for the LLM."
    (list (read-string "Rewrite prompt: " (or eca-rewrite--last-prompt
                                              eca-rewrite-prompt-prefix))))
   (eca-assert-session-running (eca-session))
-  (let* ((session (eca-session))
-         (existing-ov (eca-rewrite--overlay-at-point))
+  (let* ((existing-ov (eca-rewrite--overlay-at-point))
          (start (cond (existing-ov (overlay-start existing-ov))
                       ((use-region-p) (region-beginning))
                       (t (or (car (bounds-of-thing-at-point 'defun))
@@ -262,28 +424,48 @@ PROMPT is the instructions prompt for the LLM."
          (text (if existing-ov
                    (overlay-get existing-ov 'eca-text)
                  (buffer-substring-no-properties start end))))
-    (setq eca-rewrite--last-prompt prompt)
-    (when existing-ov
-      (eca-rewrite--reject existing-ov))
-    (eca-rewrite--send session text (buffer-file-name) start end prompt)
-    (unless (get-char-property (point) 'eca-rewrite--id)
-      (when (= (point) (region-end)) (backward-char 1)))
-    (deactivate-mark)))
+    (eca-rewrite--rewrite existing-ov (eca-session) text (buffer-file-name) start end prompt)))
 
 ;;;###autoload
-(defun eca-rewrite-reject-at-point ()
+(defun eca-rewrite-reject ()
   "Reject rewrite overlay at point."
   (interactive)
-  (when-let ((ov (eca-rewrite--overlay-at-point)))
-    (eca-rewrite--reject (list ov))))
+  (if-let ((ov (eca-rewrite--overlay-at-point)))
+    (eca-rewrite--reject (list ov))
+    (eca-error "No rewrite overlay found at point")))
 
 ;;;###autoload
-(defun eca-rewrite--accept-at-point ()
+(defun eca-rewrite-accept ()
   "Accept rewrite overlay at point."
   (interactive)
-  (when-let ((ov (eca-rewrite--overlay-at-point)))
-    (eca-rewrite--accept ov)))
+  (if-let ((ov (eca-rewrite--overlay-at-point)))
+    (eca-rewrite--accept ov)
+    (eca-error "No rewrite overlay found at point")))
 
+;;;###autoload
+(defun eca-rewrite-retry ()
+  "Retry rewrite overlay at point."
+  (interactive)
+  (eca-assert-session-running (eca-session))
+  (if-let ((ov (eca-rewrite--overlay-at-point)))
+    (eca-rewrite--retry ov (eca-session))
+    (eca-error "No rewrite overlay found at point")))
+
+;;;###autoload
+(defun eca-rewrite-diff ()
+  "Apply diff between original text and rewrite overlay at point."
+  (interactive)
+  (if-let ((ov (eca-rewrite--overlay-at-point)))
+    (eca-rewrite--diff ov)
+    (eca-error "No rewrite overlay found at point")))
+
+;;;###autoload
+(defun eca-rewrite-merge ()
+  "Merge between original text and rewrite overlay OV."
+  (interactive)
+  (if-let ((ov (eca-rewrite--overlay-at-point)))
+    (eca-rewrite--merge ov)
+    (eca-error "No rewrite overlay found at point")))
 
 (provide 'eca-rewrite)
 ;;; eca-rewrite.el ends here
